6cfef6e4ac07fd8a7a1f2d3176590237c72e1eff
Nicolas Oga <2112.oga@gmail.com>
2020-03-01 01:16:57 -0300

    Dockerize this bad boy

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:12.16.1-alpine3.11
+
+WORKDIR /personal_site
+
+ADD package.json yarn.lock
+RUN yarn install
+
+ADD . $WORKDIR
+
+CMD yarn build
--- build.sh
+++
@@ -1,3 +0,0 @@
-$(curl https://gist.githubusercontent.com/nicooga/83433ef26047383c951c7ffca1ab487a/raw/c610980baa62219967a4bf3282f4bdebede497b9/personal_site_front_environment_vars.sh)
-echo $COMMENS_API_URL
-./node_modules/webpack/bin/webpack.js -m production --progress
