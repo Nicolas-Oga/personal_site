b7666f058cc8dff3c7afbbbf99b47ceeffb302a6
Nicolas Oga <2112.oga@gmail.com>
2020-03-05 15:10:06 -0300

    Set public path

--- webpack.config.js
+++ webpack.config.js
@@ -76,7 +76,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: process.env.HOST || '/',
     filename: 'bundle.js'
   },
   devServer: {
